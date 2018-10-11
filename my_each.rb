221248ff2dfac2eec79a34c7ca27d5e0525a6f57
Sophie Gu <sophieqgu@gmail.com>
2018-10-11 03:59:47 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,10 @@
 def my_each(array)
-  if block_given?
-    count = 0 
+  
+    i = 0 
     while count < array.length 
       yield(array[i]) 
-      count += 1 
+      i += 1 
     end 
     array 
-  else 
-    puts "No block is given!"
-  end 
+  
 end
